9f518670d152225f9b9325ebf7df482fb36769d2
Hamza Ali <hamza.alee83@gmail.com>
2023-05-21 09:51:44 +0500

    Change Mongo_Uri

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 mongoose.set("strictQuery", true);
 
 const connectDB = async () => {
-   hostedUrl = "mongodb+srv://HamzaAliKundi:bcsf19e039fyp@cluster0.zl5futs.mongodb.net/"
+   hostedUrl = "mongodb://HamzaAliKundi:bcsf19e039fyp@ac-yedsc0e-shard-00-00.zl5futs.mongodb.net:27017,ac-yedsc0e-shard-00-01.zl5futs.mongodb.net:27017,ac-yedsc0e-shard-00-02.zl5futs.mongodb.net:27017/?replicaSet=atlas-umysjy-shard-0&ssl=true&authSource=admin"
    // localUrl = "mongodb://localhost:27017/jobs_available";
    try {
       const conn = await mongoose.connect(hostedUrl);
